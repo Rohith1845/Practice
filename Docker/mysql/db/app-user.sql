5a1ca0795780476c5a4803a2d48afe48967bc06b
rohith <krishnamsettyrohith@gmail.com>
2025-12-20 13:26:57 +0530

    success

--- Docker/mysql/db/app-user.sql
+++ Docker/mysql/db/app-user.sql
@@ -1,4 +1,3 @@
-CREATE USER IF NOT EXISTS 'SHIPPING'@'%'
-IDENTIFIED WITH mysql_native_password  BY 'RoboShop@1';
-GRANT ALL PRIVILEGES ON cities.* TO 'shipping'@'%';
+CREATE USER IF NOT EXISTS 'SHIPPING'@'%' IDENTIFIED WITH mysql_native_password  BY 'RoboShop@1';
+GRANT ALL ON cities.* TO 'shipping'@'%';
 FLUSH PRIVILEGES;
